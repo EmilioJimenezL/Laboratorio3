2e1e0f7444406b9d5a83f57f70c2ef69a6335ef3
emiiv <emilio.lopez@gmail.com>
2024-02-28 07:42:12 -0600

    added node counting feature for problema1ListaSimple

--- problema1ListaSimple.c
+++ problema1ListaSimple.c
@@ -4,11 +4,27 @@
  * */
 #include "stdio.h"
 #include "stdlib.h"
-int main(){
-    struct node{
-        int data;
-        struct node *link;
-    };
+
+struct node {
+    int data;
+    struct node *link;
+};
+
+void countNodes(struct node *head) {
+    int count = 0;
+    if (head == NULL) {
+        printf("La lista esta vacia");
+    }else{
+    struct node *ptr = NULL;
+    ptr = head;
+    while (ptr != NULL) {
+        count++;
+        ptr = ptr->link;
+    }
+    printf("%d", count);}
+}
+
+int main() {
     struct node *head = malloc(sizeof(struct node));
     head->data = 1;
     head->link = NULL;
@@ -19,6 +35,8 @@ int main(){
     struct node *node2 = malloc(sizeof(struct node));
     node2->data = 3;
     node2->link = NULL;
+    node1->link = node2;
     head->link->link = node2;
+    countNodes(head);
     return 0;
 }
