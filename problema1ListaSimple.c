fb52bcc0f21acc6f6846ea9f262f2f0cca1380d7
emiiv <emilio.lopez@gmail.com>
2024-02-28 07:21:13 -0600

    added linked list prototype structure

--- problema1ListaSimple.c
+++ problema1ListaSimple.c
@@ -3,10 +3,22 @@
  * Descripción breve del programa: Lista enlazada simple
  * */
 #include "stdio.h"
+#include "stdlib.h"
 int main(){
     struct node{
         int data;
         struct node *link;
     };
-    
+    struct node *head = malloc(sizeof(struct node));
+    head->data = 1;
+    head->link = NULL;
+    struct node *node1 = malloc(sizeof(struct node));
+    node1->data = 2;
+    node1->link = NULL;
+    head->link = node1;
+    struct node *node2 = malloc(sizeof(struct node));
+    node2->data = 3;
+    node2->link = NULL;
+    head->link->link = node2;
+    return 0;
 }
